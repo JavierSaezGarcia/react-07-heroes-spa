6e1471df5ba1ac7f333373a7797aed334a5012ea
javiersaezgarcia <banbleac@gmail.com>
2023-09-02 12:20:18 +0200

    gg42

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react-07-heroes-spa/' : '',
+  base: '/react-07-heroes-spa/'
  
 })
