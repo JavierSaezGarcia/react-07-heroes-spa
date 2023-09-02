8a52c65e7844b9a457e17e457e4ce23d24c21c01
javiersaezgarcia <banbleac@gmail.com>
2023-09-02 11:42:45 +0200

    issue 12

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-07-heroes-spa/'
+  base: process.env.NODE_ENV === 'production' ? '/react-07-heroes-spa/' : '',
+ 
 })
